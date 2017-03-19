5c1c4f9d93bffc56e38315cf2e21ec81abd3ff33
mill1983 <mill1983@126.com>
2017-03-20 02:06:48 +0800

    bb

---
+++ base-utils/src/main/java/mill/java/base/ListMillUtil.java
@@ -0,0 +1,10 @@
+package mill.java.base;
+
+import java.util.List;
+
+/**
+ * Created by mill on 17/3/20.
+ */
+public class ListMillUtil {
+
+}
---
+++ base-utils/src/main/java/mill/java/base/StringMillUtil.java
@@ -0,0 +1,15 @@
+package mill.java.base;
+
+/**
+ * Created by mill on 17/3/20.
+ */
+public class StringMillUtil {
+    /**
+     * 判断一个字符串是否为空
+     * @param s
+     * @return
+     */
+    public static boolean isEmpty(String s) {
+        return s==null||s.length()==0;
+    }
+}

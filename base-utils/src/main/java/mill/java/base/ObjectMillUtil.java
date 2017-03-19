d737edf962359c2a1fd72c6350dbcbf64e445a6f
mill1983 <mill1983@126.com>
2017-03-20 00:12:04 +0800

    aa

---
+++ base-utils/src/main/java/mill/java/base/ObjectMillUtil.java
@@ -0,0 +1,14 @@
+package mill.java.base;
+
+import org.apache.commons.lang.ObjectUtils;
+
+/**
+ * Created by mill on 17/3/19.
+ */
+public class ObjectMillUtil {
+
+    public static void main(String[] args) {
+
+
+    }
+}
